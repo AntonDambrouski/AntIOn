68c3df02940179e8bdfdf7d3e625dde01d7f9a5f
Anton Dambrouski <Anton.Dambrouski@itechart-group.com>
2022-09-09 17:01:10 +0200

    Small adjustments

--- Services/ChatService/Chat.Api/Program.cs
+++ Services/ChatService/Chat.Api/Program.cs
@@ -1,16 +1,11 @@
 using Chat.Api.Hubs;
-using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
