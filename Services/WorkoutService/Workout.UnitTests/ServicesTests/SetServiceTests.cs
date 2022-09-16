b51df1b1d96b8cc0b993cfb3467fbbf219c6b0d6
Anton Dambrouski <Anton.Dambrouski@itechart-group.com>
2022-09-16 14:19:21 +0200

    Add unit-tests

---
+++ Services/WorkoutService/Workout.UnitTests/ServicesTests/SetServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+using Workout.Core.Interfaces.Repositories;
+using Workout.Core.Models;
+using Workout.Core.Services;
+
+namespace Workout.UnitTests.ServicesTests;
+
+public class SetServiceTests
+{
+    private Mock<IUnitOfWork> _unitOfWork;
+    private Mock<IValidator<Set>> _validator;
+
+    public SetServiceTests()
+    {
+        _unitOfWork = new Mock<IUnitOfWork>();
+        _validator = new Mock<IValidator<Set>>();
+        _validator.Setup(v => v.Validate(It.IsAny<Set>())).Returns(new ValidationResult());
+    }
+
+    [Fact]
+    public async Task CreateAsync_SendNonExistingExerciseId_ReturnsError()
+    {
+        // Arrange
+        _unitOfWork.Setup(uof => uof.ExerciseRepository.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(null as Exercise);
+        var setService = new SetService(_unitOfWork.Object, _validator.Object);
+
+        // Act
+        var result = await setService.CreateAsync(new Set(), "bad id");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+    }
+
+    [Fact]
+    public async Task CreateAsync_SendExestingExerciseId_ReturnsNullAndCreatesSet()
+    {
+        //Arrange
+        var wasCreated = false;
+        _unitOfWork.Setup(uof => uof.ExerciseRepository.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(new Exercise());
+        _unitOfWork.Setup(uof => uof.SetRepository.CreateAsync(It.IsAny<Set>()))
+                .Returns(() =>
+                {
+                    wasCreated = true;
+                    return Task.CompletedTask;
+                });
+
+        var setService = new SetService(_unitOfWork.Object, _validator.Object);
+
+        //Act
+        var result = await setService.CreateAsync(new Set(), "existing exercise Id");
+
+        //Assert
+        Assert.Null(result);
+        Assert.True(wasCreated);
+    }
+}
---
+++ Services/WorkoutService/Workout.UnitTests/ServicesTests/TrainingServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+using Workout.Core.Interfaces.Repositories;
+using Workout.Core.Models;
+using Workout.Core.Services;
+
+namespace Workout.UnitTests.ServicesTests;
+
+public class TrainingServiceTests
+{
+    private Mock<IUnitOfWork> _unitOfWork;
+    private Mock<IValidator<Training>> _validator;
+
+    public TrainingServiceTests()
+    {
+        _unitOfWork = new Mock<IUnitOfWork>();
+        _validator = new Mock<IValidator<Training>>();
+        _validator.Setup(v => v.Validate(It.IsAny<Training>())).Returns(new ValidationResult());
+    }
+
+    [Fact]
+    public async Task CreateAsync_SendNonExistingSetId_ReturnsError()
+    {
+        // Arrange
+        var setIds = new string[] { "valid id", "valid id", "invalid id" };
+        _unitOfWork.Setup(uof => uof.SetRepository.GetByIdsAsync(It.IsAny<IEnumerable<string>>()))
+            .ReturnsAsync(setIds.Where(id => id == "valid id").Select(id => new Set()));
+        var setService = new TrainingService(_unitOfWork.Object, _validator.Object);
+
+        // Act
+        var result = await setService.CreateAsync(new Training(), setIds);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+    }
+
+    [Fact]
+    public async Task CreateAsync_SendExistingSetIds_ReturnsNullAndCreatesTraining()
+    {
+        // Arrange
+        var wasCreated = false;
+        var setIds = new string[] { "valid id", "valid id", "valid id" };
+        _unitOfWork.Setup(uof => uof.SetRepository.GetByIdsAsync(It.IsAny<IEnumerable<string>>()))
+            .ReturnsAsync(setIds.Where(id => id == "valid id").Select(id => new Set()));
+        _unitOfWork.Setup(uof => uof.TrainingRepository.CreateAsync(It.IsAny<Training>()))
+            .Returns(() =>
+            {
+                wasCreated = true;
+                return Task.CompletedTask;
+            });
+
+        var setService = new TrainingService(_unitOfWork.Object, _validator.Object);
+
+        // Act
+        var result = await setService.CreateAsync(new Training(), setIds);
+
+        // Assert
+        Assert.Null(result);
+        Assert.True(wasCreated);
+    }
+}
